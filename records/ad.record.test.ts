4bad03d92e953c7f057f56a1656698eea35834fb
Karol M <keszua@gmail.com>
2023-02-18 13:38:49 +0100

    Dodaj rekord i testy

---
+++ records/ad.record.test.ts
@@ -0,0 +1,29 @@
+import { ValidationError } from "../utils/errors";
+import { AdRecord } from "./ad.record";
+
+const defaultObj = {
+        //id: 'dfdfd',
+        name: 'Test Name',
+        description: 'blah',
+        url: 'https://megak.pl',
+        price: 0,
+        lat: 9,
+        lon: 9,
+}
+
+test('Can build AdRecord', () => {
+    const ad = new AdRecord(defaultObj);
+
+    expect(ad.name).toBe('Test Name');
+    expect(ad.description).toBe('blah');
+});
+
+test('Validates invalid price', () => {
+    expect(() => new AdRecord({
+        ...defaultObj,
+        price: -3,
+    })).toThrow('Cena nie może być mniejsza niż 0 lub większa niż 9 999 999')
+
+});
+
+//@TODO: Check all the validations
