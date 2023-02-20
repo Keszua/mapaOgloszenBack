b7b20e1441d128e1b099918a4b55d58cf6ac8cde
Karol_M <keszua@gmail.com>
2023-02-20 12:34:03 +0100

    Dodaj rate-limit

--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ import cors from 'cors';
 import 'express-async-errors';
 import { handleError, ValidationError } from "./utils/errors";
 import "./utils/db";
+import rateLimit from 'express-rate-limit'
 
 const app = express();
 
@@ -12,6 +13,12 @@ app.use(cors({
 
 app.use(json());
 
+app.use(rateLimit({
+	windowMs: 5 * 60 * 1000, // 5 minutes
+	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+}))
 
 // Routes...
 app.get('/', (req, res) => {
