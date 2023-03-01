0fa94ef7fcd98ce207462c4bc4a0b577c0cbed9d
Karol M <keszua@gmail.com>
2023-03-01 21:19:21 +0100

    Na serwer - dodanie pliku config i dodanie db.ts

---
+++ config/config.examlpe.ts
@@ -0,0 +1,12 @@
+/*
+//Chang name this file to: config.ts
+//Uncomment and remove this comment
+
+export const config = {
+	dbHost: 'you_host',
+    dbPort: 3306,
+	dbUser: 'you_name',
+	dbPassword: 'you_pass',
+	dbDatabase: 'you_database_name',
+};
+*/
